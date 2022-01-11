c3f09ee4d7ccb7855ccb8443402a21c7b5396bb3
Shaik Shahid <34955805+shaikshahid98@users.noreply.github.com>
2022-01-11 10:59:25 +0530

    Add files via upload

---
+++ myapp.js
@@ -0,0 +1,19 @@
+const joke = document.getElementById('joke');
+const btn  = document.getElementById('jokebtn');
+
+
+btn.addEventListener('click',gen_joke);
+
+gen_joke();
+function gen_joke ()
+{
+	const config = {
+		headers:{
+			Accept: 'application/json',
+		},
+	}
+	fetch('https://icanhazdadjoke.com/',config).
+	then((res) => res.json()).then((data)=>{
+		joke.innerHTML = data.joke;
+	})
+}
